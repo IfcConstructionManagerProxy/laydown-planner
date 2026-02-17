aca437b153e9878b48a47f752004ba48fb6661be
IfcConstructionManagerProxy <140843597+IfcConstructionManagerProxy@users.noreply.github.com>
2026-02-17 10:11:37 -0400

    Create DataLoader class in src/data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,17 @@
+class DataLoader:
+    def __init__(self, excel_file, schedule_file, dxf_file):
+        self.excel_file = excel_file
+        self.schedule_file = schedule_file
+        self.dxf_file = dxf_file
+
+    def load_objects_from_excel(self):
+        # Logic to load objects from Excel file
+        pass
+
+    def load_schedule_data(self):
+        # Logic to load schedule data
+        pass
+
+    def parse_dxf_laydown_maps(self):
+        # Logic to parse DXF laydown maps
+        pass
