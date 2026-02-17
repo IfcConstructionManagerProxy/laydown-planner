9f96e674d0f1bd75aa2b93c11e37c65ae1249d51
IfcConstructionManagerProxy <140843597+IfcConstructionManagerProxy@users.noreply.github.com>
2026-02-17 10:39:28 -0400

    Create setup.py file with package configuration for setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='laydown-planner',
+    version='1.0.0',
+    author='IfcConstructionManagerProxy',
+    packages=find_packages(),
+    install_requires=[
+        # To be populated with content from requirements.txt
+    ]
+)
